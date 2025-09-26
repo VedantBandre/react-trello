16f39ad3b3ab24124459df9ad433da011126a696
VedantBandre <vedantbandre33@gmail.com>
2025-09-27 01:04:55 +0200

    styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import api from '../api';
+import Note from "../components/Note";
+import '../styles/Home.css';
 
-function Home () {
+
+function Home() {
     const [notes, setNotes] = useState([]);
     const [content, setContent] = useState('');
     const [title, setTitle] = useState('');
@@ -9,57 +12,63 @@ function Home () {
     useEffect(() => {
         getNotes();
     }, []);
-    
+
     const getNotes = () => {
         api.get('/api/notes/')
-        .then((res) => res.data)
-        .then((data) => {( setNotes(data)); console.log(data) })
-        .catch((err) => alert(err));
+            .then((res) => res.data)
+            .then((data) => { (setNotes(data)); console.log(data) })
+            .catch((err) => alert(err));
     };
 
     const deleteNote = (id) => {
-        api.delete(`/api/notes/delte/${id}`).then((res) => {
+        api.delete(`/api/notes/delte/${id}/`).then((res) => {
             if (res.status == 204)
                 alert('Note Delted.');
             else
-                alert('Failed to delte note.');
+                alert('Failed to delete note.');
+            getNotes();
         }).catch((error) => alert(error))
-        getNotes();
     };
 
     const createNote = (e) => {
         e.preventDefault;
-        api.post('/api/notes/', {content, title}).then((res) => {
-            if (res.status == 201) alert('Note created.');
-            else alert('Failed to make note.');
+        api.post('/api/notes/', { content, title }).then((res) => {
+            if (res.status == 201)
+                alert('Note created.');
+            else
+                alert('Failed to make note.');
+            getNotes();
         }).catch((err) => alert(err));
-        getNotes();
     }
 
     return <div>
         <div>
-            <h2>Notes</h2>
-
+            <p className="body-note-heading">
+                <h2>Notes</h2>
+            </p>
+            {notes.map((note) => <Note note={note} onDelete={deleteNote} key={note.id} />)}
         </div>
-        <h2>Create a Note</h2>
+        <p className="body-note-heading">
+            <h2>Create a Note</h2>
+        </p>
         <form onSubmit={createNote}>
             <label htmlFor="title">Title:</label>
-            <br/>
-            <input type="text" 
-            id="title" 
-            name="title" 
-            required 
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
+            <br />
+            <input type="text"
+                id="title"
+                name="title"
+                required
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
             />
             <label htmlFor="content">Content:</label>
             <br />
-            <textarea 
-                id="content" 
-                name="content" 
-                required 
+            <textarea
+                id="content"
+                name="content"
+                required
                 value={content}
-                onChange = {(e) => setContent(e.target.value)}
+                onChange={(e) => setContent(e.target.value)}
             ></textarea>
             <br />
             <input type="submit" value='Submit'></input>
